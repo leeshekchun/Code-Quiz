4e960545bfb9c7ce76bb87cb87038a9e0b576840
ShekChun Lee <leeshekchun@gmail.com>
2022-04-08 20:03:31 -0600

    adding for loop for questions and options.

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-// "Start-Quiz button"
-var startquizBtn = document.getElementById(Start);
-
-
-// Question 01
-var set0 ={
-  question: "How many toes does a chicken feet have?",
-  options: ["Three", "Four", "Five", "Six"],
-};
-// answer: Four
-
-// question 02
-var set1 ={
-  question: "Who is bigger, Mr. Bigger, Mrs. Bigger, or their baby?",
-  options: ["Mr. Bigger", "Mrs. Bigger", "their baby"],
-};
-// answer: their baby
-
-//question 03
-var set2 ={
-  question: "How many countries in the world?",
-  options: ["150", "175", "195", "210"],
-};
-// answer: 195
-
-var allQuestion = [set0.question, set1.question, set2.question]
-
 
 // Score display
 var score = 0;
 
+  document.getElementById("Start").addEventListener("click", startquiz);
+  function startquiz(timeEl) {
+      document.getElementById("wrapper-2").innerHTML = "<div class='card-header'>test</div>";
+      clearInterval(countdown());
+  };
 
 // insert timer
 var timerEl = document.getElementById('TimerEl');
@@ -54,21 +32,41 @@ function countdown() {
     }, 1000);
   }
   countdown();
-  
-  document.getElementById("Start").addEventListener("click", startquiz);
-  function startquiz() {
-      document.getElementById("wrapper-2").innerHTML = "<div class='card-header'>test</div>";
-  };
 
+// Question 01
+var set0 ={
+  question: "How many toes does a chicken feet have?",
+  options: ["Three", "Four", "Five", "Six"],
+};
+// answer: Four
 
+// question 02
+var set1 ={
+  question: "Who is bigger, Mr. Bigger, Mrs. Bigger, or their baby?",
+  options: ["Mr. Bigger", "Mrs. Bigger", "their baby"],
+};
+// answer: their baby
 
+//question 03
+var set2 ={
+  question: "How many countries in the world?",
+  options: ["150", "175", "195", "210"],
+};
+// answer: 195
 
-// buttons for question one
-var ques1 = function () {
+var allQuestion = [set0.question, set1.question, set2.question];
+var allOptions = [set0.options[i], set1.options[i], set3.options[i]];
+
+// options div
+for (let i = 0; i < allOptions.length; i++) {
+
+}
 
 //question div
+for (let i = 0; i < allQuestion.length; i++) {
 var quesBox = document.createElement("textarea");
-
+quesBox.textContent = allQuestion[i];
+}
 
   //answer buttons container
     var buttonContainer = document.createElement("div");
@@ -76,35 +74,26 @@ var quesBox = document.createElement("textarea");
     buttonContainer.id = "button.taskid";
     //answer buttons
       var buttonAns1 = document.createElement("button");
-      buttonAns1.textContent = "Three"
+      buttonAns1.textContent = ""
       var buttonAns2 = document.createElement("button");
-      buttonAns2.textContent = "Four"
-      buttonAns2.Id = "btnP1Correct"
+      buttonAns2.textContent = ""
       var buttonAns3 = document.createElement("button");
-      buttonAns3.textContent = "Five"
+      buttonAns3.textContent = ""
       var buttonAns4 = document.createElement("button");
-      buttonAns4.textContent = "Six"
+      buttonAns4.textContent = ""
       //appendChild button buttonP1Container div
         buttonContainer.appendChild(buttonAns1);
         buttonContainer.appendChild(buttonAns2);
         buttonContainer.appendChild(buttonAns3);
         buttonContainer.appendChild(buttonAns4);
 
-        // if buttonP1Ans2 is clicked, it will show correct and jump to next page
-        if (buttonP1An2)
-        document.getElementById("btnP1Correct").addEventListener("click", ques2)
-        // if else button is clicked, it will show incorrect
-};
 
-var ques2 = function () {
+// footer for answer result        
+var footer = document.createElement("div");
+footer.id = "card-footer";
+footer.className = "card-footer";
+footer.textContent = ""
 
-};
 
-// After clicking on the "start quiz" button, function to start question
-$("startquizBtn").click(function(allQuestion)) {
-for (let i=0,; i < allQuestion.length; i++) {
-  if (allQuestion[0]);
-  $("ques1").show(function(){})
-}
 
-}
+
